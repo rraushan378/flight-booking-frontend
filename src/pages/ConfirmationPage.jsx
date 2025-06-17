8967be9597d041ef2522734c7d1cff06818bec46
rraushan378 <55614007+rraushan378@users.noreply.github.com>
2025-06-17 13:53:51 +0530

    Create ConfirmationPage.jsx

---
+++ src/pages/ConfirmationPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ConfirmationScreen from '../components/ConfirmationScreen';
+
+function ConfirmationPage() {
+  return (
+    <div>
+      <h2>Booking Confirmed</h2>
+      <ConfirmationScreen />
+    </div>
+  );
+}
+
+export default ConfirmationPage;
