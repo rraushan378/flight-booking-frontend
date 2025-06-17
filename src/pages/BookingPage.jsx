658045e99ee95bb3b01ed09ad002bb23e7b4ebb5
rraushan378 <55614007+rraushan378@users.noreply.github.com>
2025-06-17 13:53:28 +0530

    Create BookingPage.jsx

---
+++ src/pages/BookingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookingForm from '../components/BookingForm';
+
+function BookingPage() {
+  return (
+    <div>
+      <h2>Book Your Flight</h2>
+      <BookingForm />
+    </div>
+  );
+}
+
+export default BookingPage;
