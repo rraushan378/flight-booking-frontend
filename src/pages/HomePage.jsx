3b887f31d45fc3118f4e5a73ab2e06b0865d60a9
rraushan378 <55614007+rraushan378@users.noreply.github.com>
2025-06-17 13:52:22 +0530

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlightSearchForm from '../components/FlightSearchForm';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Flight Booking</h1>
+      <FlightSearchForm />
+    </div>
+  );
+}
+
+export default HomePage;
