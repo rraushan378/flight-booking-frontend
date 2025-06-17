51a8f1b383875b5cb18b2365a9bf3bbef6163730
rraushan378 <55614007+rraushan378@users.noreply.github.com>
2025-06-17 13:52:58 +0530

    Create SearchResultsPage.jsx

---
+++ src/pages/SearchResultsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlightList from '../components/FlightList';
+
+function SearchResultsPage() {
+  return (
+    <div>
+      <h2>Search Results</h2>
+      <FlightList />
+    </div>
+  );
+}
+
+export default SearchResultsPage;
